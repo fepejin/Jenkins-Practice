b8d08aa07154b2e122ce676c3c94dde7dbf79a1d
noljis <popsol95@gmail.com>
2023-04-10 11:17:18 +0900

    패키지 경로 수정

--- src/main/java/com/example/com/controller/TestController.java
+++ src/main/java/com/example/com/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.momolearn.controller;
+package com.example.com.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
+	//http://localhost/test01
 	@GetMapping(value="test01")
 	public String m1() {
 		
